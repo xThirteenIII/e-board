7149e4f2ffc77d0bb0112b8d9b61a9ededc3f212
XThirteenIII <giuseppe.richetti@gmail.com>
2026-02-14 16:00:46 +0100

    add shell structure and miniShell global var

--- shell/shell.go
+++ shell/shell.go
@@ -1,7 +1,98 @@
 package shell
 
-type Shell struct {
-	Jobs      []Job
-	Terminal  int //tty fd
-	ShellPGID int //shell process group
+import (
+	"fmt"
+	"os"
+)
+
+type shell struct {
+	fgJobs []Job
+	bgJobs []Job
+	pid    int
+	pgid   int
+}
+
+var miniShell *shell
+
+func InitMiniShell() {
+	miniShell = &shell{
+		fgJobs: make([]Job, 0, 32),
+		bgJobs: make([]Job, 0, 32),
+		pid:    os.Getpid(),
+		pgid:   os.Getgid(),
+	}
+}
+
+func GetMiniShell() *shell {
+	return miniShell
+}
+
+func (ms *shell) AddForegroundJob(j Job) error {
+	return addForegroundJob(j)
+}
+
+func AddBackgroundJob(j Job) error {
+	return addBackgroundJob(j)
+}
+
+func addForegroundJob(j Job) error {
+	if len(miniShell.fgJobs) == 32 {
+		return fmt.Errorf("Max job capacity reached")
+	}
+	miniShell.fgJobs = append(miniShell.fgJobs, j)
+	//fmt.Println("fgJobs in miniShell:", miniShell.fgJobs)
+	return nil
+}
+
+func addBackgroundJob(j Job) error {
+	if len(miniShell.fgJobs) == 32 {
+		return fmt.Errorf("Max job capacity reached")
+	}
+	miniShell.bgJobs = append(miniShell.bgJobs, j)
+	return nil
+}
+
+func GetBackgroundJobs() []Job {
+	return miniShell.bgJobs
+}
+
+func GetForegroundJobs() []Job {
+	return miniShell.fgJobs
+}
+
+// GetUniqueFgPgids returns a slice of unique pgids, from the foreground Jobs slice.
+func GetUniqueFgPgids() []int {
+
+	// If there is no foreground jobs, return nil.
+	if len(miniShell.fgJobs) == 0 {
+		return nil
+	}
+
+	// pgidsMap holds unique pgid
+	pgidsMap := make(map[int]int)
+
+	// Go through fgJobs slice.
+	// Add each foreground job pgid as a key to the map
+	// If key already exists, it will just update the value,
+	// we don't care about that
+	for i := 0; i < len(miniShell.fgJobs); i++ {
+		pgidsMap[miniShell.fgJobs[i].Pgid]++
+	}
+
+	pgids := make([]int, 0, len(pgidsMap))
+	fmt.Println("\npgid map:", pgidsMap)
+	for k, _ := range pgidsMap {
+		pgids = append(pgids, k)
+	}
+
+	return pgids
+
+}
+
+func GetMiniShellPid() int {
+	return miniShell.pid
+}
+
+func GetMiniShellPgid() int {
+	return miniShell.pgid
 }
