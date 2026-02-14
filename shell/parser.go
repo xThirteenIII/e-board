89f6adee2f3a85168f6967052b765665a57d724a
XThirteenIII <giuseppe.richetti@gmail.com>
2026-02-14 16:01:19 +0100

    add comment to parseCommand

--- shell/parser.go
+++ shell/parser.go
@@ -2,6 +2,8 @@ package shell
 
 import "strings"
 
+// parseCommand trims white spaces from the given string
+// Returns a Command with each whitespace separated arg.
 func parseCommand(s string) Command {
 	s = strings.TrimSpace(s)
 
